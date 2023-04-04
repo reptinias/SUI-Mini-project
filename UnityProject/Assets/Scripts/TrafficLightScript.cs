fe5d9daa296ce3695181aa63562f446a278ead0c
dulda280 <Sebber280@live.dk>
2023-04-04 12:58:38 +0200

    Input VR works

--- UnityProject/Assets/Scripts/TrafficLightScript.cs
+++ UnityProject/Assets/Scripts/TrafficLightScript.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
 
 public class TrafficLightScript : MonoBehaviour
 {
@@ -21,6 +24,11 @@ public class TrafficLightScript : MonoBehaviour
     private bool changeLight = false;
     private bool changeDown = true;
     private bool yellowActive = false;
+
+    public LogTracking logTracking;
+    private int frameCount;
+
+
     
     // Start is called before the first frame update
     void Start()
@@ -40,29 +48,66 @@ public class TrafficLightScript : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        if (Input.GetKey("Enter"))
+        if (Input.GetButtonDown("Fire1"))
         {
             timer = 0;
+            Debug.Log("Pressed");
+            changeLight = true;
+
         }
 
-        if (timer >= 1)
+        if (timer >= 1 && changeLight)
         {
             if (yellowActive == true)
             {
+                yellowActive = false;
+                
                 if (changeDown == true)
                 {
+                    Yellow.GetComponent<Renderer>().material.color = oldYellow;
+                    Green.GetComponent<Renderer>().material.color = LightGreen;
+                    logTracking.classID = 1;
+                    frameCount = 0;
+                    Debug.Log(logTracking.classID);
+                    changeLight = false;
+                    changeDown = false;
                     
                 }
 
                 if (changeDown == false)
                 {
-                    
+                    Yellow.GetComponent<Renderer>().material.color = oldYellow;
+                    Red.GetComponent<Renderer>().material.color = LightRed;
+                    logTracking.classID = 3;
+                    frameCount = 0;
+                    Debug.Log(logTracking.classID);
+                    changeLight = false;
+                    changeDown = true;
                 }
             }
-
+            
             if (yellowActive == false)
             {
-                
+                timer = 0;
+                Yellow.GetComponent<Renderer>().material.color = LightYellow;
+                yellowActive = true;
+                Red.GetComponent<Renderer>().material.color = oldRed;
+                Green.GetComponent<Renderer>().material.color = oldGreen;
+            }
+        }
+
+        frameCount++;
+        if (frameCount == 11)
+        {
+            if (logTracking.classID == 1)
+            {
+                logTracking.classID = 2;
+                Debug.Log(logTracking.classID);
+            }
+            if (logTracking.classID == 3)
+            {
+                logTracking.classID = 0;
+                Debug.Log(logTracking.classID);
             }
         }
     }
